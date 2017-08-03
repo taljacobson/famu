8e4292f23779031abfdb2fa3d9ad414ff0ddb3d1
taljacobson <tal1989@gmail.com>
2017-08-04 01:40:45 +0300

    deploy to firebase

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,28 @@
+// Load zone.js for the server.
+require('zone.js/dist/zone-node');
 const functions = require('firebase-functions');
+const express = require('express');
+const path = require('path')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Import renderModuleFactory from @angular/platform-server.
+const renderModuleFactory = require('@angular/platform-server').renderModuleFactory;
+
+// Import the AOT compiled factory for your AppServerModule.
+// This import will change with the hash of your built server bundle.
+const AppServerModuleNgFactory = require('./dist-server/main.bundle').AppServerModuleNgFactory;
+
+// Load the index.html file.
+const index = require('fs').readFileSync(path.resolve(__dirname, './dist-server/index.html'), 'utf8');
+
+let app = express();
+
+app.get('/', function(req, res) {
+  renderModuleFactory(AppServerModuleNgFactory, {document: index, url: '/'})
+      .then(function(html) {
+         res.send(html);
+      }).catch( function(e) {
+         console.log(e)
+      });
+});
+
+exports.ssr = functions.https.onRequest(app);
