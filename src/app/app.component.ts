cad7c1faf5b745a0bd8b235a6718f689c1a6c270
taljacobson <tal1989@gmail.com>
2017-08-15 23:11:27 +0300

    add angular material material deploy script

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
-  cunter = 0;
+  counter = 0;
+  ngOnInit() {
+    if (typeof window !== 'undefined') {
+      firebase.database().ref('counter').once('value', (e) => {
+        console.log(e.val());
+        this.counter = e.val();
+      });
+    }
+  }
 
   incNum() {
-    this.cunter++;
+    this.counter++;
+    firebase.database().ref('counter').set(this.counter);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule, MdToolbarModule} from '@angular/material';
 import { AppComponent } from './app.component';
-
+import * as firebase from 'firebase';
+import { firebaseConfig } from '../config/firebase';
+if (typeof window !== 'undefined') {
+  firebase.initializeApp(firebaseConfig);
+}
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule.withServerTransition({appId: 'my-app'}),
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
